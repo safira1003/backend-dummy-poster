e6563d35f86f01da5133d37f52c28899cb323951
Safira I A <auliasafiraisma@gmail.com>
2025-02-12 04:50:36 +0700

    Fix serverless function for Vercel

--- api/posts.js
+++ api/posts.js
@@ -14,28 +14,40 @@
 // exports.getStoredPosts = getStoredPosts;
 // exports.storePosts = storePosts;
 
-const fs = require('fs').promises;
-const path = require('path');
+// const fs = require('fs').promises;
+// const path = require('path');
 
-const filePath = path.join(__dirname, '../data/posts.json');
+// const filePath = path.join(__dirname, '../data/posts.json');
+
+// async function getStoredPosts() {
+//   try {
+//     const rawFileContent = await fs.readFile(filePath, 'utf-8');
+//     const data = JSON.parse(rawFileContent);
+//     return data.posts ?? [];
+//   } catch (error) {
+//     console.error("Error reading posts.json:", error);
+//     return [];
+//   }
+// }
+
+// async function storePosts(posts) {
+//   try {
+//     await fs.writeFile(filePath, JSON.stringify({ posts: posts || [] }));
+//   } catch (error) {
+//     console.error("Error writing posts.json:", error);
+//   }
+// }
+
+// module.exports = { getStoredPosts, storePosts };
+
+let storedPosts = [];
 
 async function getStoredPosts() {
-  try {
-    const rawFileContent = await fs.readFile(filePath, 'utf-8');
-    const data = JSON.parse(rawFileContent);
-    return data.posts ?? [];
-  } catch (error) {
-    console.error("Error reading posts.json:", error);
-    return [];
-  }
+  return storedPosts;
 }
 
 async function storePosts(posts) {
-  try {
-    await fs.writeFile(filePath, JSON.stringify({ posts: posts || [] }));
-  } catch (error) {
-    console.error("Error writing posts.json:", error);
-  }
+  storedPosts = posts;
 }
 
 module.exports = { getStoredPosts, storePosts };
